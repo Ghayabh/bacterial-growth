0c3030afc509135059b295fb4dda06f1a6844fa4
Ghaya Ben Hmidene <ghaya.benhmidene@gmail.com>
2020-11-13 16:24:59 +0100

    Ajout d'une vignette

--- R/modrosso.R
+++ R/modrosso.R
@@ -7,15 +7,16 @@
 #' @param temp time (h)
 #' @param mumax maximum growth rate # a number
 #' @param lag lag time # a number
-#' @param Nmax maximum population size #a number
-#' @param N0 initial population size # a number
+#' @param log10Nmax maximum population size #a number
+#' @param log10N0 initial population size # a number
 #'
 #' @return log10N #a number
 #' @export
 #'
 #' @examples
-#' modrosso(temp=seq(0,24,0.5),mumax=0.37,lag=4,N0=100,Nmax = 10^5)
-modrosso<-function(temp,mumax,lag,N0,Nmax){log10N<- (temp<=lag)*log10(N0)+(temp>lag)*
-  (log10(Nmax)-log10(1+((Nmax/N0)-1)*exp(-mumax*(temp-lag))))
+#' modrosso(temp=seq(0,24,0.5),mumax=0.37,lag=4,log10N0=2,log10Nmax = 5)
+modrosso<-function(temp,mumax,lag,log10N0,log10Nmax){log10N<- (temp<=lag)*log10N0+(temp>lag)*
+  (log10Nmax-log10(1+((10^log10Nmax/10^log10N0)-1)*exp(-mumax*(temp-lag))))
 return(log10N)
 }
+
--- tests/testthat/test-modrosso.R
+++ tests/testthat/test-modrosso.R
@@ -1,5 +1,5 @@
 test_that("Testing the model", {
-  expect_equal(modrosso(temp=0,mumax=0.37,lag=4,N0=100,Nmax = 10^5), 2)
-  expect_equal(modrosso(temp=15,mumax=0.37,lag=4,N0=100,Nmax = 10^5), 3.74327477251137)
-  expect_equal(modrosso(temp=1000,mumax=0.37,lag=4,N0=100,Nmax = 10^5),5)
+  expect_equal(modrosso(temp=0,mumax=0.37,lag=4,log10N0=1,log10Nmax =5), 1)
+  expect_equal(modrosso(temp=15,mumax=0.37,lag=4,log10N0=2,log10Nmax= 5), 3.74327477251137)
+  expect_equal(modrosso(temp=1000,mumax=0.37,lag=4,log10N0=2,log10Nmax = 5),5)
 })
